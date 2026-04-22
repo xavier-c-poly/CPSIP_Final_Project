819fbcf08ce3f6648f76cffad75340be13deb736
Juliana Ariza <jariza2458@floridapoly.edu>
2026-04-22 21:49:03 +0000

    Finished pull crop data. It is highly inefficient and needs to be improved but is sufficient for the moment

--- tools.py
+++ tools.py
@@ -1,8 +1,23 @@
 import requests
+import pandas as pd
 
-def download_png(url : str):
-    response = requests.get(url)
+def downloadPNG(crop : str):
+    data = pd.read_csv("stardew_data.csv")
+    crop_data = data[data["crop_name"] == crop]
+    dict = crop_data.squeeze().to_dict()
+    response = requests.get(dict["photo"])
 
     if response.status_code == 200:
         with open("crop.png", "wb") as file:
             file.write(response.content)
+
+
+def pullCropData(crop : str):
+    data = pd.read_csv("stardew_data.csv")
+    crop_data = data[data["crop_name"] == crop]
+    dict = crop_data.squeeze().to_dict()
+
+    if crop_data.empty:
+        raise ValueError
+    else:
+        return dict
